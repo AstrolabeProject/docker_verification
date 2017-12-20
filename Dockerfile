d768a2b9305372ff7ade54159b125ff195dd012b
Erik <erik.end@gmail.com>
2017-12-19 16:18:44 -0800

    dockerhub update

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:2.7-slim
 ADD fits_extract.py /
 ADD cvnidwabcut.fits /
 ADD cvnidwafcut.fits /
+ADD blank.csv /
 RUN pip install astropy
 ENTRYPOINT [ "python", "./fits_extract.py" ]
--- fits_extract.py
+++ fits_extract.py
@@ -8,7 +8,15 @@ def extract_fits(file):
 
     If it is, output csv file with header. Else, output that it is not in a log file.
     """
+    alternates_dict = {
+        'right_ascension': 'CRVAL1',
+        'declination': 'CRVAL2',
+        'spatial_axis_1_number_bins': 'NAXIS1',
+        'spatial_axis_2_number_bins': 'NAXIS2',
+        'start_time': 'DATE-OBS',
+    }
     f = fits.open(file)
+    print(file)
     with warnings.catch_warnings(record=True) as w:
         f.verify()
         if len(w) > 0:
@@ -16,9 +24,22 @@ def extract_fits(file):
             log.write("The file {} does not meet fits standard.".format(file))
         else:
             with open(file[:-5] + '.csv', 'w') as headers:
-                headers.write("name, value, description \n")
-                for key in f[0].header._cards:
-                        headers.write('{}, {}, {} \n'.format(key[0], str(key[1]).replace(",",""), key[2]))
+                template = open('blank.csv', 'r').readlines()[0]
+                headers.write(template)
+                for key in template[:-1].split(','):
+                    try:
+                        if key == 'file name or path':
+                            headers.write(file + ',')
+                        elif key in alternates_dict.keys():
+                            headers.write(str(f[0].header[alternates_dict[key]]) + ',')
+                        else:
+                            headers.write(str(f[0].header[key]) + ',')
+                    except KeyError:
+                        headers.write(',')
+                # print(f[0].header['RADESYS'])
+                # for key in f[0].header._cards:
+                #     print(key[0], str(key[1]).replace(",",""), key[2])
+                #     headers.write('{}, {}, {} \n'.format(key[0], str(key[1]).replace(",",""), key[2]))
 
 if __name__ == '__main__':
     extract_fits(sys.argv[1])
